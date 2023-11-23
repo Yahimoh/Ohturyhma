dcc95d80ddcffcee97b60e3477ff5d5c1fc6b5e3
Otso <24369914+0tso@users.noreply.github.com>
2023-11-23 13:53:30 +0200

    testit

--- src/tests/viite_test.py
+++ src/tests/viite_test.py
@@ -6,7 +6,7 @@ class ViiteTest(unittest.TestCase):
         pass
 
     def test_viite_ilmaistaan_merkkijonona(self):
-        data = {"esim": "vastaus", "esim2": "vastaus2"}
+        data = {"viite": "vastaus", "otsikko": "vastaus2", "kirjailija": "joku", "vuosi": 123, "kustantaja": "jokin"}
         viite = Viite(data)
         self.assertEqual(str, type(viite.__str__()))
 
