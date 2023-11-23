13ae313fef670d9ffb99daeb156a7c9b8c72f968
0tso <24369914+0tso@users.noreply.github.com>
2023-11-23 13:51:07 +0200

    Update viite_test.py

--- src/tests/viite_test.py
+++ src/tests/viite_test.py
@@ -11,7 +11,7 @@ class ViiteTest(unittest.TestCase):
         self.assertEqual(str, type(viite.__str__()))
 
     def test_viitteella_tieto_sanakirjana(self):
-        data = {"esim": "vastaus", "esim2": "vastaus2"}
+        data = {"viite": "vastaus", "otsikko": "vastaus2", "kirjailija": "joku", "vuosi": 123, "kustantaja": "jokin"}
         viite = Viite(data)
         self.assertEqual(dict, type(viite.tiedot))
 
