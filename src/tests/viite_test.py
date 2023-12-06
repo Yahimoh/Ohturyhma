46a513852d745ae18a84ee06b03126fa8244d66b
sampsaoinonen <sampsa.oinonen@gmail.com>
2023-12-07 00:33:02 +0200

    Lisää yksikkötestejä maarita_nimi-funktiolle

--- src/tests/viite_test.py
+++ src/tests/viite_test.py
@@ -1,4 +1,4 @@
-from src.viite import Viite
+from src.viite import Viite, maarita_nimi
 import unittest
 
 class ViiteTest(unittest.TestCase):
@@ -14,3 +14,21 @@ class ViiteTest(unittest.TestCase):
         data = {"viite": "blabla", "tyyppi": "article", "otsikko": "vastaus2", "kirjailija": "joku", "vuosi": 123, "kustantaja": "jokin"}
         viite = Viite(data)
         self.assertEqual(dict, type(viite.tiedot))
+
+    def test_maarita_nimi_yksi_tekija(self):
+        self.assertEqual(maarita_nimi("Joe Smith", 2020), "S20")
+
+    def test_maarita_nimi_monta_tekijaa(self):
+        self.assertEqual(maarita_nimi("Sam Smith, Pekka Pouta and Joe Doe", 2023), "SPD23")
+
+    def test_maarita_nimi_tekijat_erotettu_eri_tavoilla(self):
+        self.assertEqual(maarita_nimi("Sam Smith, Pekka Pouta and Joe Doe ja Tapsa Jormakka", 2021), "SPDJ21")
+    
+
+
+    
+    
+    
+    
+
+    
