6e075917b8f0daa7289b3793d1efef7b0d95f761
sampsaoinonen <sampsa.oinonen@gmail.com>
2023-12-13 13:35:58 +0200

    Lisää validators testit

--- src/validators.py
+++ src/validators.py
@@ -1,4 +1,3 @@
-from flask import flash
 from src.viite import Viite
 
 class SyoteVirhe(Exception):
@@ -11,11 +10,10 @@ def syotteen_tarkastus(viite: Viite):
         arvo = str(arvo)
         if avain == "tyyppi":
             continue
-        if len(arvo) > 5 or len(arvo) < 100:
+        if len(arvo) < 1 or len(arvo) > 100:
             virheet.append(f"Kentän '{avain}' syöte tulee olla vähintään 1 ja enintään 100 merkkiä pitkä!")    
 
     if virheet:
         raise SyoteVirhe(virheet)
             
     return True
-        
